68d61edfea6a8e5a790f2591458052ee1964a021
quan <quanld@haposoft.com>
2025-06-20 11:16:58 +0700

    ep9 done

--- app/Http/Resources/V1/UserResource.php
+++ app/Http/Resources/V1/UserResource.php
@@ -20,12 +20,17 @@ class UserResource extends JsonResource
             'attributes'=>[
                 'name'=>$this->name,
                 'email'=>$this->email,
-                $this->mergeWhen($request->routeIs('user.*'),[
+                $this->mergeWhen($request->routeIs('users.*'),[
                     'emailVerifiedAt'=>$this->email_verified_at,
                     'createdAt'=>$this->created_at,
                     'updatedAt'=>$this->updated_at,
                 ])
-            ]
+            ],
+            'includes'=>TicketResource::collection($this->whenLoaded('tickets')),
+            'links'=>[
+                'self'=>route('users.show',['user'=>$this->id])
+            ],
+
         ];
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 
 class User extends Authenticatable
@@ -47,5 +49,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function tickets(): HasMany
+    {
+        return $this->hasMany(Ticket::class);
     }
 }
